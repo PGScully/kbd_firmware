fd908be6c224bf93048de65dd2a03fb78ada705c
Peter Scully <peter@peterscully.name>
2025-08-27 10:59:35 +1000

    Use #undef/#define to set max rgb brightness

--- keyboards/crkbd/vial-kb/vial-qmk/keymaps/vial_mini/config.h
+++ keyboards/crkbd/vial-kb/vial-qmk/keymaps/vial_mini/config.h
@@ -27,9 +27,9 @@
 // keyboard responsiveness) #   define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits
 // in milliseconds how frequently an animation will update the LEDs. 16 (16ms)
 // is equivalent to limiting to 60fps (increases keyboard responsiveness)
-// #define RGB_MATRIX_MAXIMUM_BRIGHTNESS
-//   150 // limits maximum brightness of LEDs to 150 out of 255. Higher may cause
-//       // the controller to crash.
+#undef RGB_MATRIX_MAXIMUM_BRIGHTNESS
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150 // limits maximum brightness of LEDs to 150 out of 255. Higher may cause
+// the controller to crash.
 #define RGB_MATRIX_HUE_STEP 8
 #define RGB_MATRIX_SAT_STEP 8
 #define RGB_MATRIX_VAL_STEP 8
